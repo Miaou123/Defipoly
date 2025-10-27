6367c7adde091387dd38c77a63f2863bb7c2068c
Rengon0x <olivier.bertau123@gmail.com>
2025-10-28 00:42:16 +0100

    tailwind setup fix

--- defipoly-frontend/tailwind.config.js
+++ defipoly-frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -21,7 +22,6 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      // ADD THIS:
       keyframes: {
         'slide-in-right': {
           '0%': { transform: 'translateX(100%)', opacity: '0' },
